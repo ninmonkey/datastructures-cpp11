c99df16e3a3adc80c78270971c536c55c3c1776c
Jake Bolton <ninmonkeys@gmail.com>
2013-09-17 13:54:01 -0500

    shared_ptr

--- linked list/linked list/main.cpp
+++ linked list/linked list/main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <memory>
+#include <string>
+
 using namespace std;
 /* todo
 
 	- best c++ naming cases is?
-	- gcc -Wall -Wextra or whatever else
-	- use MAKE_SHARED vs shared_ptr ?
+	- gcc =c++11/0x -Wall -Wextra or whatever else
+	- use `std::make_shared` vs shared_ptr ?
 	- lint
 
+	- I could have : singly list use unique_ptr but doubly use shared_ptr
 
 About:
 	Singly linked list. For production code use something like std::forward_list
+
+
+make_shared
+
+	rather than:
+		std::shared<string> foo(new string("foo"));
+
+	consider: ( http://stackoverflow.com/questions/14836691/is-it-better-to-use-shared-ptr-reset-or-operator/14837300#14837300 )
+		std::shared<string> foo = std::make_shared<string>("foo");
+		
+	therefore, an assignment using `make_shared<>` is preferable (or even a statement such as
+		reset(make_shared<T>())
+			// or? make_shared<T>(args...).swap(mysharedptr);
 */
 
 /*
@@ -21,20 +38,41 @@ class LinkedList {
 
 struct Node {
 	string data;
-	Node *next;
+	std::shared_ptr<Node> next;
 
-	Node() : data("default"), next(nullptr) { }
+	Node() : data("default") { }
+};
+
+class LinkedList {
+	std::shared_ptr<Node> head;
+	int _size;
+public:
+	LinkedList() : _size(0) { }	
+
+	int size() { return _size; }
 };
 
 int main() {
-	
-	Node * head = nullptr;
-	Node n = Node();
-	head = &n;
+	//auto p = std::make_shared<string>();
 
-	if(head)
-		cout << "yes" << endl;	
-	
+
+	std::shared_ptr<Node> n0 = std::make_shared<Node>();
+	auto n1 = std::make_shared<Node>();
+
+	std::shared_ptr<Node> head(n0);
+
+	n0->data = "1";
+	n1->data = "2";
+
+	cout << n0->data << endl;
+	cout << n1->data << endl;
+
+
+
+
+
+
+
 	cout << "Done." << endl;	
 	cin.get();
 	return 0;
