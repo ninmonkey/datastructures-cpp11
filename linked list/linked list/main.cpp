f55907e418809e722d708988052c4739a4dc99fc
Jake Bolton <ninmonkeys@gmail.com>
2013-09-20 00:48:46 -0500

    append/prepend work, but new list.clear() error

--- linked list/linked list/main.cpp
+++ linked list/linked list/main.cpp
@@ -92,6 +92,7 @@ public:
 class LinkedList {
 	int size_;
 	std::shared_ptr<Node> head;
+	//std::shared_ptr<Node> tail; // not used at the moment for simplicity
 
 public:
 	LinkedList() : size_(0) {}
@@ -101,10 +102,22 @@ public:
 
 	// Add node to start of list
 	void prepend( string const& node );
+	// Add node to end of list
+	void append( string const& node );
 	// is empty?
 	bool empty() const { return size() == 0; }
 	// free it
 	void clear() {
+		auto cur = head;
+
+		// should be free'ing cur->prev if it were doubly linked
+		while(cur != nullptr) {
+			auto prev = cur;
+			cur = cur->next;
+			prev->next = nullptr;
+			prev = nullptr;
+			size_ -= 1;
+		}
 		//todo: delete head/set to nullptr func
 		//size_ = 0;
 	}
@@ -118,8 +131,8 @@ public:
 
 // Add node to start of list
 void LinkedList::prepend( string const& datum ) {
-	size_ += 1;
-
+	size_ += 1;	
+	
 	auto new_node = make_shared<Node>();
 	new_node->datum = datum;
 	
@@ -128,20 +141,45 @@ void LinkedList::prepend( string const& datum ) {
 		new_node->next = head;
 
 	head = new_node;
+	
+	//todo: just do?
+	//insert_before( head, datum );
 }
 
+// Add node to start of list
+void LinkedList::append( string const& datum ) {
+	size_ += 1;	
+	
+	auto new_node = make_shared<Node>();
+	new_node->datum = datum;
+
+	auto cur = head;
+	while(cur->next != nullptr)
+		cur = cur->next;
+
+	//cur->next is nullptr
+	cur->next = new_node;
+	new_node->next = nullptr; // default means this is already set
+
+	//todo: just do?
+	//insert_after(tail, datum );
+}
+
+/*
 void LinkedList::insert_before( std::shared_ptr<Node>, string const& datum)
-{
+{	
 
-}
+
+}*/
 
 // string for debug
 void LinkedList::str() const {
 	cout << "list [" << size() << "] = \t";
 	//todo: as c++11 for-each loop
 	auto cur = head;
-	while(cur != nullptr) 
-	{
+
+	//or: for( auto cur = head; cur != nullptr; cur = cur->next ) {  }
+	while(cur != nullptr) {
 		cout << cur->datum << ", ";
 		cur = cur->next;
 	}
@@ -155,14 +193,12 @@ void test_print(LinkedList const& list) {
 // generate basic list for test
 LinkedList test_build3() {
 	LinkedList list = LinkedList();
-	// ask crit: ok to: append(make_shared<...>) or memory issue?
-	//auto n1 = make_shared<Node>();
-	//auto n2 = make_shared<Node>();
-	//auto n3 = make_shared<Node>();
-	
+
 	list.prepend("fred");
 	list.prepend("bob");
 	list.prepend("hank");
+	list.append("sally");
+	list.append("jane");
 
 	return list;
 }
@@ -172,6 +208,9 @@ int main() {
 	//LinkedList list = LinkedList();
 
 	LinkedList list = test_build3();
+	test_print(list);
+
+	list.clear();
 	test_print(list);
 	
 
